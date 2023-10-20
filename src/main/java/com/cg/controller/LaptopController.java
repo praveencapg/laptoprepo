35751b703e99c99135fccc1e74ba71254ab55992
praveen10 <psuravarapu10@gmail.com>
2023-10-20 10:54:50 +0530

    Controller class commit

---
+++ src/main/java/com/cg/controller/LaptopController.java
@@ -0,0 +1,35 @@
+package com.cg.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.Entity.Laptop;
+import com.cg.service.LaptopService;
+
+
+@RestController
+@RequestMapping("/laptopapi")
+public class LaptopController {
+	
+	
+	
+	
+	@Autowired
+	private LaptopService lservice;
+	
+	@PostMapping("/")
+	public String addLaptop(@RequestBody Laptop lp) {
+		return lservice.addLaptop(lp);
+	}
+	
+	public List<Laptop> getAll(@RequestParam("id") int id){
+		return lservice.getallLaptops();
+	
+	}
+}
